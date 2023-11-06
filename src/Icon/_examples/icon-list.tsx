00d7f0f7c12aead2aca22354093fb9aed4b8a55f
HouKunLin <houkunlin@aliyun.com>
2023-11-06 20:01:56 +0800

    feat(icon): 增加 Icon 组件

---
+++ src/Icon/_examples/icon-list.tsx
@@ -0,0 +1,11 @@
+import { Icon } from 'layui';
+import React from 'react';
+
+export default () => {
+  return (
+    <>
+      <Icon icon={'left'} />
+      <Icon icon={'release'} />
+    </>
+  );
+};
---
+++ src/Icon/index.md
@@ -0,0 +1,5 @@
+# Icon 图标
+
+## 图标列表
+
+<code src="./_examples/icon-list.tsx"></code>
