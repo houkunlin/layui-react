642ecf019f8cfe4b77009ef3bd27eaa1c6229656
HouKunLin <houkunlin@aliyun.com>
2023-11-07 09:45:52 +0800

    feat(Icon): 完善 Icon 组件和文档

--- src/Icon/_examples/icon-list.tsx
+++ src/Icon/_examples/icon-list.tsx
@@ -1,11 +1,44 @@
-import { Icon } from 'layui';
-import React from 'react';
+import { Icon, IconItemList } from 'layui';
+import React, { useEffect, useState } from 'react';
+import './styles.less';
+
+function getIconUnicode(iconClass: any) {
+  const iconEl = document.querySelector(`.site-doc-icon > li > .${iconClass}`);
+  if (!iconEl) {
+    return '';
+  }
+  return window?.getComputedStyle(iconEl)?.content;
+}
 
 export default () => {
+  const [iconsUnicode, setIconsUnicode] = useState<string[]>([]);
+
+  useEffect(() => {
+    const code: string[] = [];
+    IconItemList?.forEach((icon) => {
+      // unicode 10进制转16进制
+      const unicode = getIconUnicode('layui-icon-' + icon.class)
+        .charCodeAt(1)
+        .toString(16);
+      code.push(unicode);
+    });
+    setIconsUnicode(code);
+  }, []);
+
   return (
-    <>
-      <Icon icon={'left'} />
-      <Icon icon={'release'} />
-    </>
+    <ul className="site-doc-icon">
+      {IconItemList.map((item, index) => {
+        const icon = item.class;
+
+        return (
+          <li key={icon}>
+            <Icon icon={icon} />
+            <div className="doc-icon-name">{item.name}</div>
+            <div className="doc-icon-code">&amp;#x{iconsUnicode[index]};</div>
+            <div className="doc-icon-fontclass">{icon}</div>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
